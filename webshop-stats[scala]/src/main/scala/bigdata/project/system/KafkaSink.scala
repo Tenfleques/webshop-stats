3911fcf56bc1766073529135d458dd355b59c893
Tenfleques <tendaichikake@gmail.com>
2018-04-01 20:16:49 +0300

    slightly working scala version

--- webshop-stats[scala]/src/main/scala/bigdata/project/system/KafkaSink.scala
+++ webshop-stats[scala]/src/main/scala/bigdata/project/system/KafkaSink.scala
@@ -2,6 +2,7 @@ package bigdata.project.system
 import java.util.Properties
 
 import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.spark.rdd.RDD
 
 class KafkaSink(brokers : String) {
   val props = new Properties()
@@ -14,15 +15,19 @@ class KafkaSink(brokers : String) {
   props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
   props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
   val producer = new KafkaProducer[String,String](props)
+  def writeToKafka(rdd : RDD [(String,Long)]): Unit ={
+    val  props = new Properties()
+    props.put("bootstrap.servers","localhost:9092")
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
 
-  def write(topic :String, value: String): Unit = {
-    /*producer.send(new ProducerRecord[String, String](topic, value)) {
-    }*/
+    val producer = new KafkaProducer[String, String](props)
+    rdd.foreach(record => {
+      print(record._1+"=>"+record._2.toString)
+      //producer.send(new ProducerRecord[String, String]("refree-stats-out",record._1,record._2.toString))
+    })
   }
   def done(): Unit ={
     producer.close()
   }
-
-
-
 }
