7b59d678abc866c84b3b0b9a4fb5c63788f3028d
Tenfleques <tendaichikake@gmail.com>
2018-04-18 15:43:17 +0300

    make bootstrap, add docker file

---
+++ configure.env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo export KAFKA_HOME=/home/blackjack/dev/apache/kafka_2.11-1.0.1
+echo export ZOOKEEPER_HOME=/home/blackjack/dev/apache/kafka_2.11-1.0.1/
+echo export WSERVER_HOME=/home/blackjack/dev/apache/kafka_2.11-1.0.1/apps/webshop-stats/dist/
